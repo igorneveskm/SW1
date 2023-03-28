4bdf4df8c6e0444f5b6266232da28d27c8180dd0
igor neves silva <100931488+igorneveskm@users.noreply.github.com>
2023-03-28 12:38:09 -0300

    proj pessoa

---
+++ projeto_pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa ("GODOFREDO", 18);
+       // p1.AlterarNome("IGOR");
+       // p1.AlterarSobreNome("SILVA");
+       // p1.AlterarIdade(16);
+        p1.MostraDados();
+    }
+}
