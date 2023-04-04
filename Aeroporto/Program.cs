652b22d02358ac68fca1a2b5f185f9f39aff85e2
igor neves silva <100931488+igorneveskm@users.noreply.github.com>
2023-04-04 12:21:59 -0300

    aeroporto

---
+++ Aeroporto/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aeroporto;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aeroporto a1 = new Aeroporto();
+        a1.AlteraCodigo(1);
+        a1.AlteraNome("Gol");
+        a1.MostraDados();
+
+    }
+}
