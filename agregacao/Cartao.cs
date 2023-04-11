7733a1fa3cd53de7d4e1b7d27ac4f343acc20c68
igor neves silva <100931488+igorneveskm@users.noreply.github.com>
2023-04-11 12:03:03 -0300

    agregação

---
+++ agregacao/Cartao.cs
@@ -0,0 +1,7 @@
+﻿namespace agregacao;
+class Cartao
+{
+    public string? Numero {get; set;}
+    public string? DataValidade {get; set;}
+    public Cliente? Cliente {get; set;}
+}
---
+++ agregacao/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace agregacao;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Trabalhando com agregação");
+        
+        CartaoDeCredito cdc = new CartaoDeCredito();
+        Cliente cli = new Cliente();
+        
+        cli.Nome = "Igor";
+
+        cdc.Numero = "123654789";
+        cdc.DataValidade = "01/2027";
+
+         cdc.Cliente = cli;
+
+        Console.WriteLine("O nome do cliente é: " + cli.Nome);
+        Console.WriteLine("O número do cartão é: " + cdc.Numero);
+        Console.WriteLine("A data de validade é: " + cdc.DataValidade);
+        Console.WriteLine("O nome do cliente AGREGADO é: " + cdc.Cliente.Nome);
+    }
+}
