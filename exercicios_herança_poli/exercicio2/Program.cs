9a76cbd9077b2b990fa8d26e22109a6ee80d03d6
igor neves silva <100931488+igorneveskm@users.noreply.github.com>
2023-05-26 16:17:28 -0300

    Add files via upload

---
+++ exercicios_herança_poli/exercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace exercicio2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Fisico f = new Fisico();
+       Juridico j = new Juridico();
+       
+       Console.WriteLine("Escolha a opção para contrato: JURIDICO ou FISICO.");
+        string metrica = Console.ReadLine();
+
+         switch (metrica)
+        {
+            case "física":
+                Console.WriteLine(f.Dados());
+                break;
+
+            case "juridica":
+                Console.WriteLine(j.Dados());
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
+
+    }
+}
+
