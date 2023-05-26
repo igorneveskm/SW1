684931e7bc6d3f543568448bc60d67d4028e9e6b
igor neves silva <100931488+igorneveskm@users.noreply.github.com>
2023-05-26 11:10:51 -0300

    exercicio 2

---
+++ exercicio2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace exercicio2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Fisico f = new Fisico();
+       Juridico j = new Juridico();
+       
+       Console.WriteLine(f.Dados());
+}}
